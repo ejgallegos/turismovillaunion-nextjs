e13de7302c1ca86f2da65a8d61da49bfdcb17f1a
Galí Estudio <galiestudiodev@gmail.com>
2025-06-29 14:53:33 +0000

    por defecto activa el modo claro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
