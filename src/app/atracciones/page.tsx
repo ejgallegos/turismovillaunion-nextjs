24981e57d6e2abd57ec4ba7639587b82e55796d9
Galí Estudio <galiestudiodev@gmail.com>
2025-07-01 20:03:34 +0000

    ejecuta npm run build

--- src/app/atracciones/page.tsx
+++ src/app/atracciones/page.tsx
@@ -1,15 +1,5 @@
-import { Atractivos } from '@/components/landing/attractions';
-import { Footer } from '@/components/landing/footer';
-import { Header } from '@/components/landing/header';
+import { notFound } from 'next/navigation';
 
-export default function AtractivosPage() {
-  return (
-    <div className="flex min-h-screen flex-col">
-      <Header />
-      <main className="flex-1">
-        <Atractivos />
-      </main>
-      <Footer />
-    </div>
-  );
+export default function AtraccionesPage() {
+  notFound();
 }
