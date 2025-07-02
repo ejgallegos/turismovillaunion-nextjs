1cc9c7ac6455127bda72d4575be07f417f6539d7
Galí Estudio <galiestudiodev@gmail.com>
2025-07-02 13:16:20 +0000

    vamos con otro error
    Error occurred prerendering page "/admin". Read mo

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,7 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
+export const dynamic = 'force-dynamic';
+
 export default function AdminDashboard() {
   return (
     <div>
