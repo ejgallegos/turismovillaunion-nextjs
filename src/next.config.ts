93a1d7a3b743bafe584d31a06d5363d7d6432874
Galí Estudio <galiestudiodev@gmail.com>
2025-07-05 14:32:07 +0000

    en la pagina principal tambien me devuelve 404

---
+++ src/next.config.ts
@@ -0,0 +1,27 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'standalone',
+  serverActions: {
+    bodySizeLimit: '10mb',
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
