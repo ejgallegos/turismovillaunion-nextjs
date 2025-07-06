c9cff88ddd52f9c35b5403ecdc62dc70162390e0
Eric Gallegos <ejgallegos@gmail.com>
2025-07-06 10:17:43 -0300

    Ajustes docker y nextjs.config

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,17 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  serverActions: {
-    bodySizeLimit: '10mb',
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+	serverActions: {
+		bodySizeLimit: "10mb",
+	},
+	typescript: {
+		// !! WARN !!
+		// Dangerously allow production builds to successfully complete even if
+		// your project has type errors.
+		// !! WARN !!
+		ignoreBuildErrors: true,
+	},
+	output: "standalone",
 };
 
 export default nextConfig;
